28bdb2f4b251c6f9a5e9f0ae71713f360acdef01
Никита Бравцев <nmbravtsev@sberbank.ru>
2022-06-16 17:07:13 +0300

    ru.job4j.Multiple

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+     public static void main (String[] args){
+         for (int i = 0; i < 10; i++) {
+             System.out.println(1 + "*" + i + "=" + i);
+         }
+
+         }
+     }
+
