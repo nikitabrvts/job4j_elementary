e1bbdbb632b303ac7baabb36bdf7c6d0c73c7980
Никита Бравцев <nmbravtsev@sberbank.ru>
2022-06-17 15:38:41 +0300

    calculator x100

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,22 +2,12 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int four = 4;
-        int five = 5;
-        int six = 6;
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
 
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+    public static void main(String[] args) {
+        int result3 = Calculator.func1(100);
     }
 }
-
