a13f6b144f541c615bbef0667fe60c52e658aee7
Никита Бравцев <nmbravtsev@sberbank.ru>
2022-06-17 12:06:14 +0300

    calc_new

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,13 +1,6 @@
 package ru.job4j;
 
 public class Multiple {
-    int age = 18;
-
-    float length = (float) 78.0332;
-
-    boolean baby = true;
-
-
      public static void main(String[] args) {
          for (int i = 0; i < 10; i++) {
              System.out.println(1 + "*" + i + "=" + i);
